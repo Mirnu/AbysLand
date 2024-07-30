28a8ca6ebf13b099438dba8b6ff2a6e2ac9a4ee4
Kiriil <kirillprofi007@gmail.com>
2024-07-30 17:42:48 +0300

    ft: added stages

--- Assets/Scripts/World/BiomeFeature.cs
+++ Assets/Scripts/World/BiomeFeature.cs
@@ -1,5 +1,4 @@
 using System;
-using UnityEngine.Tilemaps;
 
 namespace Assets.Scripts.World {
     [Serializable]
--- Assets/Scripts/World/Generators/UpperWorldGen.cs
+++ Assets/Scripts/World/Generators/UpperWorldGen.cs
@@ -74,44 +74,10 @@ namespace Assets.Scripts.World {
 
         //Ugly ass nigga
         private void GenerateCorners() {
-            map[map.GetUpperBound(0)/2, map.GetUpperBound(0)-1] = 2;
-            map[map.GetUpperBound(0)/2, map.GetUpperBound(0)/2] = 2;
-            map[0, map.GetUpperBound(1)/2] = 2;
-            map[map.GetUpperBound(0) - 1, map.GetUpperBound(1)/2] = 2;
-            GenerateLine(map.GetUpperBound(0)/2, map.GetUpperBound(0)/2, map.GetUpperBound(0)/2, map.GetUpperBound(1)-1);
-            GenerateLine(map.GetUpperBound(0)/2, map.GetUpperBound(0)/2, 0, map.GetUpperBound(1)/2);
-            GenerateLine(map.GetUpperBound(0)/2, map.GetUpperBound(0)/2, map.GetUpperBound(0), map.GetUpperBound(1)/2);
-            FillAreaFromCorner(99, 99, 2, 1);
-            FillAreaFromCorner(0, 99, 2, 3);
+            
         }
 
-        private void GenerateLine(int x0, int y0, int x1, int y1) {
-            var l = x0 > x1 ? -1 : 1;
-            var l1 = y0 > y1 ? -1 : 1;
-            int j = y0;
-            int i = x0;
-            while (i != x1 || j != y1) {
-                map[i, j] = 2;
-                if(j != y1) {
-                    j += l1;
-                }
-                if(i != x1) {
-                    i+=l;
-                }
-            }
-        }
-
-        private void FillAreaFromCorner(int startX, int startY, int borderIndex, int fillIndex) {
-            var b = startY < map.GetUpperBound(0) - 1 ? 1 : -1;
-            var a = startX < map.GetUpperBound(0) - 1 ? 1 : -1;
-            for(int i = startX; i < map.GetLength(0); i+=a) {
-                if(map[i, startY] == borderIndex) { break; }
-                for(int j = startY; j < map.GetLength(0); j+=b) {
-                    if (map[i, j] == borderIndex) { break; }
-                    map[i, j] = fillIndex;
-                }
-            }
-        }
+        
 
         #endregion
 
