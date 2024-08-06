892875e7952779086401e558a0fc0afefa3555d1
Kiriil <kirillprofi007@gmail.com>
2024-08-06 15:04:06 +0300

    ft: added saving

---
+++ Assets/Scripts/Saving/SavingManager.cs
@@ -0,0 +1,34 @@
+﻿using Assets.Scripts.Misc.Saving;
+using Zenject;
+
+namespace Assets.Scripts.Saving
+{
+    public class SavingManager : ILateDisposable
+    {
+
+        public T GetData<T>() where T : new()
+        {
+            if (Repository.TryGetData(out T data))
+            {
+                return data;
+            }
+            return new T();
+        }
+
+        [Inject]
+        private void PostInject()
+        {
+            Repository.LoadState();
+        }
+
+        public void LateDispose()
+        {
+            Repository.SaveState();
+        }
+
+        public void SaveData<T>(T data) where T : new()
+        {
+            Repository.SetData(data);
+        }
+    }
+}
